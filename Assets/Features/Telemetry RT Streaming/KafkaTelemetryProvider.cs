fdf518bd6a8047b17167006d0da95016d0479a05
Antreas Christofi <victoryspecificationii@gmail.com>
2024-07-02 18:45:48 +0300

    creating unified script for telemetry

--- Assets/Features/Telemetry RT Streaming/KafkaTelemetryProvider.cs
+++ Assets/Features/Telemetry RT Streaming/KafkaTelemetryProvider.cs
@@ -4,13 +4,37 @@ using VehiclePhysics;
 using VehiclePhysics.InputManagement;
 using EdyCommonTools;
 using Newtonsoft.Json;
+using Confluent.Kafka;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Perrinn424
 {
 
 public class KafkaTelemetryProvider : VehicleBehaviour{
 	
-	public bool emitTelemetry = true;
+
+    [Serializable]
+    public class Settings{
+        
+        public string bootstrapServers = "10.144.0.2:9092";
+        public string topic = "test-topic";
+        //public string message = "Hello, Kafka!";
+        
+    	}
+
+		public bool emitTelemetry = true;
+    	public Settings settings = new Settings();            
+        private KafkaTelemetryConnector connector;
+
+	void Awake (){
+
+        Debug.Log("Kafka Telemetry Experiment Started");	
+        connector = new KafkaTelemetryConnector(settings.bootstrapServers);
+
+        }
 
 	public override void OnEnableVehicle ()
 		{
@@ -43,7 +67,7 @@ public class KafkaTelemetryProvider : VehicleBehaviour{
 	public class Perrinn424Distance : Telemetry.ChannelGroup
 		{
 
-		public override void PollValues (float[] values, int index, Object instance)
+		public override void PollValues (float[] values, int index, UnityEngine.Object instance)
 			{
 			VehicleBase vehicle = instance as VehicleBase;
 
@@ -54,4 +78,64 @@ public class KafkaTelemetryProvider : VehicleBehaviour{
 		}
 	}
 
+    public class KafkaTelemetryConnector : VehicleBehaviour
+    {
+        private string bootstrapServers;
+        private IProducer<string, string> producer;
+        VehicleBase vehicle;
+        
+        public KafkaTelemetryConnector(string bootstrapServers)
+        {
+            this.bootstrapServers = bootstrapServers;
+        }
+
+        private string DataRowToJson()
+        {
+            string json = "test!!";
+            return json;
+        }
+
+
+        public async Task ConnectAndSendAsync(string topic/*, string message*/)
+        {
+            var config = new ProducerConfig { BootstrapServers = bootstrapServers };
+            
+            // Create a new producer instance
+            using (var producer = new ProducerBuilder<string, string>(config).Build())
+            {
+                try
+                {
+                    string jsonMessage = DataRowToJson();
+                    // Construct the message to send
+                    var kafkaMessage = new Message<string, string>
+                    {
+                        Value = jsonMessage
+                    };
+
+                    // Produce the message to the specified topic
+                    var deliveryResult = await producer.ProduceAsync(topic, kafkaMessage);
+
+                    // Log the delivery result
+                    Debug.Log($"Message delivered to {deliveryResult.TopicPartitionOffset}");
+                }
+                catch (ProduceException<string, string> e)
+                {
+                    Debug.Log($"Delivery failed: {e.Error.Reason}");
+                }
+            }
+        }
+    }
 }
+
+
+/*
+Yes, that's the Telemetry system in the vehicle. 
+When you have a reference to the vehicle controller, then vehicleController.telemetry.latest gives you the latest recorded datarow with all the channel values. 
+vehicleController.telemetry.channelIndex is an index of the channel names and their indexes. 
+With the channel index, you can get the value of a channel from the data row, as well as get channel information. 
+vehicleController.telemetry.channels is the array with the information on each channel.
+
+You may search the GitHub repo for references on when the .telemetry field is accessed.
+
+Feel free to request more information on the PERRINN forum. That's the best place to exchange this kind of detailed information.
+*/
