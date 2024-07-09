bf54fbd238bcb1d02dca5e177a147fb5a5964685
Antreas Christofi <victoryspecificationii@gmail.com>
2024-07-09 12:47:42 +0300

    removed constructors, added stuff in the awake function

--- Assets/Features/Telemetry RT Streaming/KafkaTelemetryClient.cs
+++ Assets/Features/Telemetry RT Streaming/KafkaTelemetryClient.cs
@@ -9,59 +9,69 @@ using System.Linq;
 using VehiclePhysics;
 using Newtonsoft.Json;
 
-namespace Perrinn424{
-
-    public class KafkaTelemetryClient : VehicleBehaviour{
-
+namespace Perrinn424
+{
+    public class KafkaTelemetryClient : MonoBehaviour
+    {
         [Serializable]
-        public class Settings{
-        
+        public class Settings
+        {
             public string bootstrapServers = "10.144.0.2:9092";
             public string topic = "test-topic";
-            //public string message = "Hello, Kafka!";
-        
         }
 
-
-    	public bool kafkaEnabled = true;
-        public Settings settings = new Settings();            
+        public bool kafkaEnabled = true;
+        public Settings settings = new Settings();
         private KafkaTelemetryConnector connector;
-        
 
-
-        void Awake (){
-
-            Debug.Log("Kafka Telemetry Experiment Started");	
-            connector = new KafkaTelemetryConnector(settings.bootstrapServers);
-
+        void Awake()
+        {
+            Debug.Log("Kafka Telemetry Experiment Started");
+            connector = gameObject.AddComponent<KafkaTelemetryConnector>();
+            connector.bootstrapServers = settings.bootstrapServers;
         }
 
-        void onEnableVehicle (){
+        void OnEnable()
+        {
             Debug.Log("OnEnabledVehicle Reached");
         }
 
-        void Update (){
-            //The Update method in Unity cannot be async, because it is called once per frame and should not be awaited.
-            // Instead, we call an asynchronous method SendKafkaMessageAsync from within Update.        
-            SendKafkaMessageAsync();
-
+        void Update()
+        {
+            if (kafkaEnabled)
+            {
+                SendKafkaMessageAsync();
+            }
         }
 
-        private async void SendKafkaMessageAsync(){
-            try{
-                await connector.ConnectAndSendAsync(settings.topic/*, settings.message*/);
+        private async void SendKafkaMessageAsync()
+        {
+            try
+            {
+                await connector.ConnectAndSendAsync(settings.topic);
                 Debug.Log("Message sent to Kafka!");
             }
-            catch (Exception e){
+            catch (Exception e)
+            {
                 Debug.LogException(e, this);
             }
         }
 
-        public override void OnDisableVehicle (){
+        void OnDisable()
+        {
             Debug.Log("Kafka Telemetry Experiment Stopped");
         }
-
     }
 }
 
+/*
+Yes, that's the Telemetry system in the vehicle. 
+When you have a reference to the vehicle controller, then vehicleController.telemetry.latest gives you the latest recorded datarow with all the channel values. 
+vehicleController.telemetry.channelIndex is an index of the channel names and their indexes. 
+With the channel index, you can get the value of a channel from the data row, as well as get channel information. 
+vehicleController.telemetry.channels is the array with the information on each channel.
 
+You may search the GitHub repo for references on when the .telemetry field is accessed.
+
+Feel free to request more information on the PERRINN forum. That's the best place to exchange this kind of detailed information.
+*/
--- Assets/Features/Telemetry RT Streaming/KafkaTelemetryConnector.cs
+++ Assets/Features/Telemetry RT Streaming/KafkaTelemetryConnector.cs
@@ -1,40 +1,38 @@
-
-using EdyCommonTools;
 using Confluent.Kafka;
 using System;
 using System.Threading.Tasks;
 using UnityEngine;
-using System.Collections.Generic;
-using System.Linq;
 using VehiclePhysics;
 using Newtonsoft.Json;
 
 namespace Perrinn424
 {
-    public class KafkaTelemetryConnector : VehicleBehaviour
+    public class KafkaTelemetryConnector : MonoBehaviour
     {
-        private string bootstrapServers;
+        public string bootstrapServers;
         private IProducer<string, string> producer;
-        VehicleBase vehicle;
-        
-        public KafkaTelemetryConnector(string bootstrapServers)
+        private VehicleBase vehicle;
+
+        void Awake()
         {
-            this.bootstrapServers = bootstrapServers;
+            vehicle = GetComponent<VehicleBase>();
         }
 
         private string DataRowToJson()
         {
-            string json = "test!!";
+            // Assuming vehicle.telemetry.latest is a DataRow object
+            // and you have a method to convert it to a JSON string
+
+            string json = JsonConvert.SerializeObject(vehicle.telemetry.latest);
             return json;
         }
 
-
-        public async Task ConnectAndSendAsync(string topic/*, string message*/)
+        public async Task ConnectAndSendAsync(string topic)
         {
             var config = new ProducerConfig { BootstrapServers = bootstrapServers };
-            
+
             // Create a new producer instance
-            using (var producer = new ProducerBuilder<string, string>(config).Build())
+            using (producer = new ProducerBuilder<string, string>(config).Build())
             {
                 try
                 {
@@ -59,16 +57,3 @@ namespace Perrinn424
         }
     }
 }
-
-
-/*
-Yes, that's the Telemetry system in the vehicle. 
-When you have a reference to the vehicle controller, then vehicleController.telemetry.latest gives you the latest recorded datarow with all the channel values. 
-vehicleController.telemetry.channelIndex is an index of the channel names and their indexes. 
-With the channel index, you can get the value of a channel from the data row, as well as get channel information. 
-vehicleController.telemetry.channels is the array with the information on each channel.
-
-You may search the GitHub repo for references on when the .telemetry field is accessed.
-
-Feel free to request more information on the PERRINN forum. That's the best place to exchange this kind of detailed information.
-*/
