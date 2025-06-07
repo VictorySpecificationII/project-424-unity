ee7cd0c1a9cae36eaaffe0f575dd74a64a48fd8d
VictorySpecificationII <antreas@christofi.me>
2025-06-07 14:53:09 +0300

    Implement Kafka telemetry batching to send aggregated data once per second
    - Changed telemetry polling to collect all channel data once per second
    - Batched telemetry messages into a single JSON payload per second
    - Improved network efficiency by reducing Kafka message frequency and size
    - Fixed message size issues by consolidating channel data into one message
    - Added configurable batch interval (hardcoded to 1 second for now)

--- Assets/Features/Telemetry RT Streaming/KafkaTelemetry2.cs
+++ Assets/Features/Telemetry RT Streaming/KafkaTelemetry2.cs
@@ -1,220 +1,138 @@
-
 using UnityEngine;
 using VehiclePhysics;
 using VehiclePhysics.InputManagement;
 using EdyCommonTools;
 using Newtonsoft.Json;
 using Confluent.Kafka;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Perrinn424
 {
+    public class KafkaTelemetry2 : VehicleBehaviour
+    {
+        public bool emitTelemetry = true;
 
-public class KafkaTelemetry2: VehicleBehaviour
-	{
-	public bool emitTelemetry = true;
+        // Time control for one-second interval sending
+        private float lastSendTime = 0f;
+        private readonly float sendInterval = 1f;
 
+        public override void OnEnableVehicle()
+        {
+            Debug.Log("[KafkaTelemetry2] Vehicle enabled: Kafka telemetry component active.");
+        }
 
-	public override void OnEnableVehicle ()
-		{
+        public override bool EmitTelemetry()
+        {
+            return emitTelemetry;
+        }
 
-		}
+        public override void RegisterTelemetry()
+        {
+            vehicle.telemetry.Register<Perrinn424Distance>(this);
+            Debug.Log("[KafkaTelemetry2] Registered Perrinn424Distance telemetry group.");
+        }
 
+        public override void UnregisterTelemetry()
+        {
+            vehicle.telemetry.Unregister<Perrinn424Distance>(this);
+            Debug.Log("[KafkaTelemetry2] Unregistered Perrinn424Distance telemetry group.");
+        }
 
-	public override bool EmitTelemetry ()
-		{
-		return emitTelemetry;
-		}
+        public class Perrinn424Distance : Telemetry.ChannelGroup
+        {
+            public override int GetChannelCount()
+            {
+                // Adjust to your actual channel count if needed
+                return 2;
+            }
 
+            public override Telemetry.PollFrequency GetPollFrequency()
+            {
+                // Use Normal since Slow is unavailable
+                return Telemetry.PollFrequency.Normal;
+            }
 
-	public override void RegisterTelemetry ()
-		{
-		vehicle.telemetry.Register<Perrinn424Distance>(vehicle);
-		}
+            public override void GetChannelInfo(Telemetry.ChannelInfo[] channelInfo, Object instance)
+            {
+                // Optional channel info
+            }
 
+            public override void PollValues(float[] values, int index, Object instance)
+            {
+                KafkaTelemetry2 kafkaTelemetry = instance as KafkaTelemetry2;
+                if (kafkaTelemetry == null) return;
 
-	public override void UnregisterTelemetry ()
-		{
-		vehicle.telemetry.Unregister<Perrinn424Distance>(vehicle);
-		}
+                // Only send once every 1 second
+                if (Time.time - kafkaTelemetry.lastSendTime < kafkaTelemetry.sendInterval)
+                {
+                    // Skip sending this poll
+                    return;
+                }
+                kafkaTelemetry.lastSendTime = Time.time;
 
-	public class Perrinn424Distance : Telemetry.ChannelGroup
-		{
-		public override int GetChannelCount ()
-			{
-			return 2;
-			}
+                VehicleBase vehicle = kafkaTelemetry.vehicle;
+                int numChannels = vehicle.telemetry.latest.values.Length;
 
+                var batch = new List<object>(numChannels);
 
-		public override Telemetry.PollFrequency GetPollFrequency ()
-			{
-			return Telemetry.PollFrequency.Normal;
-			}
+                for (int i = 0; i < numChannels; i++)
+                {
+                    Telemetry.ChannelInfo channelInfo = vehicle.telemetry.channels[i];
+                    Telemetry.ChannelGroupInfo groupInfo = channelInfo.group;
 
+                    var telemetryMessage = new
+                    {
+                        Timestamp = Time.time,
+                        ChannelId = i,
+                        ChannelName = channelInfo.name,
+                        ChannelValue = vehicle.telemetry.latest.values[i],
+                        ChannelUnit = vehicle.telemetry.GetChannelSemmantic(i).displayUnitsSuffix,
+                        ChannelMinValue = vehicle.telemetry.GetChannelSemmantic(i).displayRangeMin,
+                        ChannelMaxValue = vehicle.telemetry.GetChannelSemmantic(i).displayRangeMax,
+                        ChannelMultiplier = vehicle.telemetry.GetChannelSemmantic(i).displayMultiplier,
+                        ChannelGroup = groupInfo.channels.ToString(),
+                        ChannelCount = groupInfo.channelCount,
+                        ExpectedFrequency = groupInfo.expectedFrequency.ToString(),
+                        ActualFrequency = groupInfo.actualFrequency,
+                        UpdateInterval = groupInfo.updateInterval,
+                        FrequencyLabel = groupInfo.updateFrequencyLabel,
+                        Semantic = channelInfo.semantic.ToString()
+                    };
 
-		public override void GetChannelInfo (Telemetry.ChannelInfo[] channelInfo, Object instance)
-			{
-			VehicleBase vehicle = instance as VehicleBase;
+                    batch.Add(telemetryMessage);
+                }
 
-			// // Custom distance semantic
+                if (kafkaTelemetry.emitTelemetry)
+                {
+                    string jsonBatch = JsonConvert.SerializeObject(batch, Formatting.Indented);
+                    _ = SendKafkaMessageAsync(jsonBatch);
+                }
 
-			// var distanceSemantic = new Telemetry.SemanticInfo();
-			// distanceSemantic.SetRangeAndFormat(0, 21000, "0.000", " km", multiplier:0.001f, quantization:1000);
+                // Optional dummy telemetry output
+                // values[index + 0] = 0.0f;
+                // values[index + 1] = 0.0f;
+            }
+        }
 
-			// // Fill-in channel information
+        private static async Task SendKafkaMessageAsync(string message)
+        {
+            var config = new ProducerConfig { BootstrapServers = "192.168.1.243:9092" };
 
-			// channelInfo[0].SetNameAndSemantic("LapDistance", Telemetry.Semantic.Custom, distanceSemantic);
-			// channelInfo[1].SetNameAndSemantic("TotalDistance", Telemetry.Semantic.Custom, distanceSemantic);
-			}
-
-
-		public override void PollValues (float[] values, int index, Object instance)
-			{
-			VehicleBase vehicle = instance as VehicleBase;
-
-			int numChannels = vehicle.telemetry.latest.values.Length;
-			for (int i = 0; i <= numChannels -1; i++){
-
-			
-			/*
-			in game telemetry screen
-			channel number
-			semantic
-			group
-			update frequency
-			channel minimum value
-			channel maximum value
-			latest value
-
-			Usage Notes
-		    // Get the value for the fourth channel.
-
-		    //float channelValue = vehicle.telemetry.latest.values[3];
-
-		    // Get the string with the units that may be appended to the value. It may include leading spaces.
-
-		    //string channelUnits = vehicle.telemetry.GetChannelSemmantic(12).displayUnitsSuffix;
-
-		    // Alternatively, we can get a formatted string with both value and units.
-		    //string channelValueWithUnits = vehicle.telemetry.FormatChannelValue(3);
-
-			*/
-			//UNDER DEVELOPMENT
-
-			// Retrieve ChannelInfo object
-			Telemetry.ChannelInfo channelInfo = vehicle.telemetry.channels[i];
-
-			// Retrieve ChannelGroupInfo object
-			Telemetry.ChannelGroupInfo groupInfo = channelInfo.group;
-
-			string channel_group = groupInfo.channels.ToString();
-			int channel_count = groupInfo.channelCount;
-			string expectedFreq = groupInfo.expectedFrequency.ToString();
-			float actualFreq = groupInfo.actualFrequency;
-			int interval = groupInfo.updateInterval;
-			string frequencyLabel = groupInfo.updateFrequencyLabel;
-
-
-			// //Output or use the values as needed
-			// Debug.Log($"Channel ID: {i}");
-			// Debug.Log($"Channel Group: {channel_group}");
-			// Debug.Log($"Channel Count: {channel_count}");
-			// Debug.Log($"Expected Frequency: {expectedFreq}");
-			// Debug.Log($"Actual Frequency: {actualFreq}");
-			// Debug.Log($"Update Interval: {interval}");
-			// Debug.Log($"Frequency Label: {frequencyLabel}");
-
-			// Access the semantic information
-			Telemetry.Semantic semantic = channelInfo.semantic;
-			// Telemetry.SemanticInfo customSemantic = channelInfo.customSemantic;//dont use for now, until you figure it out
-
-			// Log or use the semantic information
-			//Debug.Log($"Semantic: {semantic}");
-			// Debug.Log($"Custom Semantic: {customSemantic}");
-
-			var channel_id = i;
-			var channel_name = vehicle.telemetry.channels[i].name;
-			var channel_value = vehicle.telemetry.latest.values[i];
-			var channel_unit = vehicle.telemetry.GetChannelSemmantic(i).displayUnitsSuffix;
-			var channel_min_value = vehicle.telemetry.GetChannelSemmantic(i).displayRangeMin;
-			var channel_max_value = vehicle.telemetry.GetChannelSemmantic(i).displayRangeMax;
-			var channel_multiplier = vehicle.telemetry.GetChannelSemmantic(i).displayMultiplier;
-
-			// Debug.Log($"Channel Name: {channel_name}");
-			// Debug.Log($"Channel Value: {channel_value}");
-			// Debug.Log($"Channel_Unit: {channel_unit}");
-			// Debug.Log($"Channel Min: {channel_min_value}");
-			// Debug.Log($"Channel Max: {channel_max_value}");
-			// Debug.Log($"Channel Multiplier: {channel_multiplier}");
-
-			//LEGACY
-			// Debug.Log($"Channel Name: {vehicle.telemetry.channels[i].name}"); //Get name of channel
-		    // Debug.Log($"Channel Value: {vehicle.telemetry.latest.values[i]}"); // Get value of channel
-			// Debug.Log($"Channel Unit: {vehicle.telemetry.GetChannelSemmantic(i).displayUnitsSuffix}"); //Get unit of channel
-			// Debug.Log($"Channel Minimum: {vehicle.telemetry.GetChannelSemmantic(i).displayRangeMin}"); // Get minimum value
-			// Debug.Log($"Channel Maximum: {vehicle.telemetry.GetChannelSemmantic(i).displayRangeMax}"); // Get maximum value
-			// Debug.Log($"Channel Multiplier: {vehicle.telemetry.GetChannelSemmantic(i).displayMultiplier}"); // Get multiplier
-			// Construct the telemetry message using the variables
-			var telemetryMessage = new
-			{
-				ChannelId = channel_id,
-				ChannelName = channel_name,
-				ChannelValue = channel_value,
-				ChannelUnit = channel_unit,
-				ChannelMinValue = channel_min_value,
-				ChannelMaxValue = channel_max_value,
-				ChannelMultiplier = channel_multiplier,
-				ChannelGroup = channel_group,
-				ChannelCount = channel_count,
-				ExpectedFrequency = expectedFreq,
-				ActualFrequency = actualFreq,
-				UpdateInterval = interval,
-				FrequencyLabel = frequencyLabel,
-				Semantic = semantic.ToString()
-			};
-
-			string jsonMessage = JsonConvert.SerializeObject(telemetryMessage, Formatting.Indented);
-			//Debug.Log(jsonMessage);
-			//SendKafkaMessageAsync(jsonMessage);
-
-			}
-
-			//Telemetry.DataRow latest = vehicle.telemetry.latest;
-            //string json = JsonConvert.SerializeObject(latest);
-            //Debug.Log(json);
-			//SendKafkaMessageAsync(json);
-
-			// values[index+0] = (float)latest.distance;
-			// values[index+1] = (float)latest.totalDistance;
-			}
-		}
-
-		private static async Task SendKafkaMessageAsync(string message)
-		{
-			var config = new ProducerConfig { BootstrapServers = "192.168.1.243:9092" };
-
-			// Create a new producer instance
-			using (var producer = new ProducerBuilder<Null, string>(config).Build())
-			{
-				try
-				{
-					// Construct the message to send
-					var kafkaMessage = new Message<Null, string> { Value = message };
-
-					Debug.Log($"Message content: {kafkaMessage.Value}");
-
-					// Produce the message to the specified topic
-					var deliveryResult = await producer.ProduceAsync("my-topic", kafkaMessage);
-
-					// Log the delivery result
-					Debug.Log($"Message delivered to {deliveryResult.TopicPartitionOffset}");
-				}
-				catch (ProduceException<Null, string> e)
-				{
-					Debug.Log($"Delivery failed: {e.Error.Reason}");
-				}
-			}
-		}
-	}
-
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    var kafkaMessage = new Message<Null, string> { Value = message };
+                    Debug.Log($"[KafkaTelemetry2] Sending batch to Kafka:\n{message}");
+                    var deliveryResult = await producer.ProduceAsync("p424-telemetry-batch", kafkaMessage);
+                    Debug.Log($"[KafkaTelemetry2] Batch delivered to {deliveryResult.TopicPartitionOffset}");
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    Debug.LogError($"[KafkaTelemetry2] Delivery failed: {e.Error.Reason}");
+                }
+            }
+        }
+    }
 }
